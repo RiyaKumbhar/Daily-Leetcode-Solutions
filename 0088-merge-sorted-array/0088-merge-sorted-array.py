f026b7c1a09ad07567690ee34728a7301ec9de6e
RiyaKumbhar <86878196+RiyaKumbhar@users.noreply.github.com>
2025-12-18 21:32:44 -0700

    Time: 0 ms (100%), Space: 17.2 MB (100%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -5,12 +5,10 @@ class Solution:
         """
         last = m + n - 1
 
-        # merge in reverse order
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
-                 nums1[last] = nums1[m - 1]
-                 m -= 1
-            
+                nums1[last] = nums1[m - 1]
+                m -= 1
             else:
                 nums1[last] = nums2[n - 1]
                 n -= 1
@@ -18,6 +16,6 @@ class Solution:
         
         while n > 0:
             nums1[last] = nums2[n - 1]
-            n, last = n - 1, last - 1
+            last, n = last - 1, n - 1
 
 
