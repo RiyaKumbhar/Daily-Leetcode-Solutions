209ac4f90e039312ff17aa442914c1549558016f
RiyaKumbhar <86878196+RiyaKumbhar@users.noreply.github.com>
2025-12-21 14:41:17 -0700

    Time: 0 ms (100%), Space: 17.2 MB (99.7%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -4,10 +4,7 @@ class Solution:
             return 0
         
         for i in range(len(haystack) + 1 - len(needle)):
-            for j in range(len(needle)):
-                if haystack[i + j] != needle[j]:
-                    break
-                if j == len(needle) - 1:
+            if haystack[i: i + len(needle)] == needle:
                     return i
         return -1
         
