8502d50186fe935044abded06e8be3e1d86233f3
RiyaKumbhar <86878196+RiyaKumbhar@users.noreply.github.com>
2026-01-21 23:54:55 -0700

    Time: 4 ms (46.92%), Space: 20.5 MB (21.21%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        total_sum = n * (n + 1) // 2
-        actual_sum = sum(nums)
-        return total_sum - actual_sum
+        xorr = n
+
+        for i in range(n):
+            xorr ^= i ^ nums[i]
+        return xorr
